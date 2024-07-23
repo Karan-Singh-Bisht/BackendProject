2b4f06fac16d85e86630f41689b603979762dc8a
Karan <karansbisht7@gmail.com>
2024-07-23 21:35:50 +0530

    Created routes for user controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,13 @@ const {
   loginUser,
   logOutUser,
   refreshAccessToken,
+  changeUserPassword,
+  getCurrentUser,
+  updateUserInfo,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelInfo,
+  getWatchHistory,
 } = require("../controllers/user.controller.js");
 const upload = require("../middlewares/multer.middleware.js");
 const { verifyJWT } = require("../middlewares/auth.middleware.js");
@@ -21,5 +28,22 @@ router.post(
 router.post("/login", loginUser);
 router.post("/logout", verifyJWT, logOutUser);
 router.post("/refresh-token", refreshAccessToken);
+router.post("/changePassword", verifyJWT, changeUserPassword);
+router.get("/current-user", verifyJWT, getCurrentUser);
+router.patch("/update-user", verifyJWT, updateUserInfo);
+router.patch(
+  "/update-avatar",
+  verifyJWT,
+  upload.single("avatar"),
+  updateUserAvatar
+);
+router.patch(
+  "/update-coverImage",
+  verifyJWT,
+  upload.single("coverImage"),
+  updateUserCoverImage
+);
+router.get("/c/:userName", verifyJWT, getUserChannelInfo);
+router.get("/history", verifyJWT, getWatchHistory);
 
 module.exports = router;
