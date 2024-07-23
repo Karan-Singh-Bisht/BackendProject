ec083526bd8d5723028ba18e50c60ef738bf5841
Karan <karansbisht7@gmail.com>
2024-07-23 21:21:35 +0530

    Added Watch History

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -342,3 +342,50 @@ module.exports.getUserChannelInfo = asyncHandler(async (req, res) => {
     .status(200)
     .json(new apiResponse(200, channel[0], "user channel found!"));
 });
+
+module.exports.getWatchHistory = asyncHandler(async (req, res) => {
+  const user = userModel.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    userName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner", //first element nikalne ke liye
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(new apiResponse(200, user[0].watchHistory, "Watch History Fetched"));
+});
